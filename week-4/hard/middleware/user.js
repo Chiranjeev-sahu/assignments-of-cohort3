7fa1ae5f2252e0ea8d942094e222195675f194a2
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2025-04-11 18:36:45 +0530

    Implemented Auth middleware

--- week-4/hard/middleware/user.js
+++ week-4/hard/middleware/user.js
@@ -1,5 +1,28 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-}
+import jwt from 'jsonwebtoken';
+const SECRET = 'SecretKey' 
 
-module.exports = userMiddleware;
+export const authenticateJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1]; 
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.userId = user.id;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+//Authorization MiddleWare
+export const isAdmin = (req, res, next) => {
+  if (req.user.role === 'admin') {
+    next();
+  } else {
+    return res.status(403).json({ message: 'Unauthorized: Insufficient role' });
+  }
+};
