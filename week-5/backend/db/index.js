a018519d790a33a82998032f7df5932acf331dd0
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2025-04-19 12:00:00 +0530

    wrote the auth middleware

--- week-5/backend/db/index.js
+++ week-5/backend/db/index.js
@@ -1,1 +1,19 @@
-//  start writing from here
+const mongoose=require('mongoose');
+
+mongoose.connect('url_to_be_inserted');
+
+const UserSchema=new mongoose.Schema({
+
+})
+
+const TodoSchema=new mongoose.Schema({
+
+});
+
+const User=mongoose.model('User',UserSchema);
+const Todo=mongoose.model('Todo',TodoSchema);
+
+module.exports={
+    User,
+    Todo
+}
